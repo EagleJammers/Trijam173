6c04228614aa54779a39ecce10162acdfd094334
Clockknight <34818025+Clockknight@users.noreply.github.com>
2022-06-12 14:43:43 -0700

    Completed Player.Move()

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +16,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Move();
     }
 
     void Move()
     {
-      Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
+      Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+      this.transform.position += input * speed * Time.deltaTime;
     }
 }
